3db63b0e42b3411c084e6b34aa34b6a7b78a8427
Ro <lichtseeker@gmail.com>
2022-06-18 20:28:27 +0800

    Add answer buttons.
    Add answer buttons.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -6,12 +6,26 @@ void main() {
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    var questions = [
+      'I like to do things the same way.',
+      'I would rather hang out alone rather than go to a party with friends.'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz Now'),
         ),
-        body: Text('This is a brief autism quiz.'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Never'),
+              onPressed: null,
+            ),
+            RaisedButton(child: Text('Some of the time'), onPressed: null),
+            RaisedButton(child: Text('Most of the time'), onPressed: null),
+          ],
+        ),
       ),
     );
   }
