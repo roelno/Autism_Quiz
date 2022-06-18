b57eeb9f91f17ca7d4326c56e6b3dac601290881
Ro <lichtseeker@gmail.com>
2022-06-18 16:20:20 +0800

    Hello
    Hello

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,24 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: Text('Hello'),
     );
   }
 }
