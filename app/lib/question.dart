190234ade488a885bcc0c8155205377bcb7c1627
Ro <lichtseeker@gmail.com>
2022-06-19 16:33:20 +0800

    Update main after a new widget is split out.
    Update the main.dart code after split the 'Question' into a separate widget.

--- app/lib/question.dart
+++ app/lib/question.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Question extends StatelessWidget {
   String questionText;
 
-  Question(this.questionText)
+  Question(this.questionText);
   @override
   Widget build(BuildContext context) {
     return Text(questionText);
