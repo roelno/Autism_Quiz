f6e4cfeb801b1d7dba311bba36c4374e34026089
Ro <lichtseeker@gmail.com>
2022-06-20 09:51:23 +0800

    Add some styling to the the 'Answer'.
    Also update the main.dart.

--- app/lib/answer.dart
+++ app/lib/answer.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
+  final VoidCallback selectButton;
+
+  Answer(this.selectButton);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
         color: Colors.blue,
+        textColor: Colors.white,
         child: Text('Never'),
-        onPressed: null,
+        onPressed: selectButton,
       ),
     );
   }
